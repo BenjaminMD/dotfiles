acbc41a3939794658eea98d6207a47b4f03edfe0
Benjamin Fahl <Benjamin.Fahl@rwth-aachen.de>
2023-05-03 10:10:45 +0200

    borders and titles removed

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,20 +7,28 @@ vim.cmd([[
     set expandtab 
     set number
     set relativenumber
+    set smartindent
     set clipboard=unnamedplus
     set wrap
     set linebreak
     set showbreak=..
     set wrapmargin=2
+    set scrolloff=8
+    set signcolumn=yes
 ]])
+
+
+
+--vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 require('plugins')
 require'lspconfig'.pyright.setup{}
 require('configs.lsp')
 
 if vim.g.neovide == true then
-  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.01<CR>", { silent = true })
-  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.01<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>", { silent = true })
+  vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>", { silent = true })
   vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
 end
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { silent = true })
@@ -28,4 +36,19 @@ vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true,
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.jukit_shell_cmd = 'conda activate py311 && ipython3'
+
+
+vim.cmd([[
+    highlight ColorColumn ctermbg=0 guibg=#212f3d
+]])
+
+
+
+-- remaps
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("x", "<leader>p", [["_dP]])
+--
+
 
--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -44,10 +44,17 @@ require('lspconfig')['texlab'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+
 require('lspconfig')['tsserver'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+
+require('lspconfig')['bashls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
 require('lspconfig')['rust_analyzer'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
